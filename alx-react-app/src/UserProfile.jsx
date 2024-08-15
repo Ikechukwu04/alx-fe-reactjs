55dc7bdfecae6c0d793eddc511718063f38b6dae
Ikechukwu04 <163460439+Ikechukwu04@users.noreply.github.com>
2024-08-15 18:57:23 +0100

    Add files via upload

---
+++ alx-react-app/src/UserProfile.jsx
@@ -0,0 +1,9 @@
+const UserProfile = (props) => {
+    return (
+        <div><UserProfile 
+        name="Ike" age="25" 
+        bio="Loves coding and writing" /></div>
+    );
+  };
+
+  export default UserProfile;
