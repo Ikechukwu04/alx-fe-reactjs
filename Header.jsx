3f3e0fb7dd80f7cdd41d653ef622a77d9dc41a11
Ikechukwu04 <163460439+Ikechukwu04@users.noreply.github.com>
2024-08-18 11:12:34 +0100

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <Header style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}>
+      <h1>My Favorite Cities</h1>
+    </Header>
+  );
+};
+
+export default Header;
