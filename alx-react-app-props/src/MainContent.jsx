bd47503f9c6b1638e3aa1464b562b13e47cd80a4
Ikechukwu04 <ikechukwugodwinokafor@gmail.com>
2024-08-25 13:01:58 +0100

    second commit

--- alx-react-app-props/src/MainContent.jsx
+++
@@ -1,9 +0,0 @@
-const MainContent = () => {
-  return (
-    <main style={{ padding: '20px', backgroundColor: '#f0f0f0', color: '#333', textAlign: 'center' }}>
-      <p>I love to visit New York, Paris, and Tokyo.</p>
-    </main>
-  );
-};
-
-export default MainContent;
