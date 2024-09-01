781ac506484783511a7f20ba00d0d644e23cbe6d
Ikechukwu04 <163460439+Ikechukwu04@users.noreply.github.com>
2024-09-01 07:53:08 +0100

    Add files via upload

---
+++ react-query-demo/src/PostsComponent.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { useQuery } from 'react-query';
+import axios from 'axios';
+
+const fetchPosts = async () => {
+  const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+  return response.data;
+};
+
+const PostsComponent = () => {
+  const { data, error, isLoading, isError, refetch } = useQuery('posts', fetchPosts);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return (
+    <div>
+      <button onClick={() => refetch()} style={{ marginBottom: '20px' }}>
+        Refetch Posts
+      </button>
+      <ul>
+        {data.map(post => (
+          <li key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default PostsComponent;
