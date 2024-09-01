4921819d52e3e2256ad5295e0d443409cfc60223
Ikechukwu04 <163460439+Ikechukwu04@users.noreply.github.com>
2024-09-01 08:12:38 +0100

    Update PostsComponent.jsx

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -6,6 +6,9 @@ const fetchPosts = async () => {
   const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
   return response.data;
 };
+{
+  "cacheTime", "staleTime", "refetchOnWindowFocus", "keepPreviousData"
+}
 
 const PostsComponent = () => {
   const { data, error, isLoading, isError, refetch } = useQuery('posts', fetchPosts);
