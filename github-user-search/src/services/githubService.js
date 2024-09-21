86c6f234f1cfedfda14cfc362cbbbd0f2d0f291a
Ikechukwu04 <163460439+Ikechukwu04@users.noreply.github.com>
2024-09-22 00:22:49 +0100

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const BASE_URL = 'https://api.github.com/users';
 
-export const fetchUserData = async (username, location, repos) => {
+export const fetchAdvancedUserData = async (username, location, repos) => {
   const response = await axios.get(`${BASE_URL}/${username}?q=${query}&page=${page}`);
   let query = username ? `${username} in:login` : '';
   if (location) {
